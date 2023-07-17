10d5b4a8acd05c4d6ad97d306f32099d29cf1dd1
Marcin Adamiuk <adamiuk.marcin@gmail.com>
2023-07-17 11:13:34 +0200

    Variables - zmienne

---
+++ src/sripts/notes.js
@@ -0,0 +1,68 @@
+// Tworzenie zmiennej
+// var, let , const
+
+// let i const się nie da
+// let x = 42;
+// let x = 2137
+// console.log(x);
+
+// Reinicjalizacja (var, let)
+// var x = 42;
+// x = 2137;
+// console.log(x);
+//
+// let y = 42
+// y = 2137;
+// console.log(y);
+//
+// // const się nie da
+// const z = 42;
+// z = 2137;
+// console.log(z);
+
+
+// Hoisting - przenoszenie deklaracji zmiennych, funkcji i klas na górę, aktualnie
+// przetwarzanie zasięgu. (var, let (TDZ), const (TDZ))
+// TDZ - temporary dead zone
+
+// console.log(x);
+// var x = 42;
+//
+// var y;
+// console.log(x);
+// x = 42;
+
+// console.log(add(1, 2));
+//
+// funktion add(a, b){
+//     return a + b;
+// }
+
+// console.log(x);
+// let x = 42;
+
+// Zasięg (scope) - widoczność, dostępnośc identyfikatorów (nazwy zwiennych, funkcji i klas),
+// tzn. gdzie to można uzywać
+
+// funkcyjny - ograniczony przez ciało funkcji (var)
+// blokowy - ograniczony przez pare klamerek (curly braces) (let, const)
+
+// const x = 10
+// if (x) {
+//     var y = 42;
+//     let y1 = 42;
+//     const y2 = 42;
+// }
+//
+// console.log(y);
+// console.log(y1);
+// console.log(y2);
+
+// function magic(){
+//     var y = 42;
+//     let y1 = 42;
+//     const y2 = 42;
+// }
+//
+// magic();
+// console.log(y, y1, y2);
