43da77c6be54d9982824cec09ebe1ed6b56fe61b
Marcin Adamiuk <adamiuk.marcin@gmail.com>
2023-07-18 15:00:16 +0200

    Oop style - constructor

--- src/scripts/autoFn.js
+++ src/scripts/autoFn.js
@@ -1,56 +1,73 @@
 // Program do tworzenia samochodów
 // samochód ma brand, prędkość, silnik, max prędkość
-// samochód może przyśpieszać, zwalniać, odpalić silnik i gasić
+// samochód może przyśpieszać, zwalniać, odpalać silnik i gasić
 
-
-
-function Auto(brand, maxSpeed){
-    this.brand = brand;
-    this.speed = 0;
-    this.maxSpeed = maxSpeed;
-    this.engine = false;
-}
-
-const bmw = new Auto('bmw', 140);
-const mercedes = new Auto ('mercedes', 190);
-Auto.prototype.startEngine = function(){
-    if(!this.engine){
-        this.engine = true;
-        console.log('silnik odpalony')
-    } else {
-        console.log('silnik już był odpalony')
-    }
-}
-
-Auto.prototype.stopEngine = function(){
-    if(this.speed === 0){
+class Auto {
+    constructor(brand, maxSpeed) {
+        this.brand = brand;
+        this.speed = 0;
+        this.maxSpeed = maxSpeed;
         this.engine = false;
-        console.log('silnik zgaszony')
-    } else {
-        console.log('zatrzymaj się, zanim zgasisz silnik');
     }
-}
 
-Auto.prototype.speedUp = function(amount){
-    if(this.engine) {
-        this.speed = Math.min(this.speed + amount, this.maxSpeed);
+    startEngine() {
+        if (!this.engine) {
+            this.engine = true;
+            console.log('silnik odpalony')
+        } else {
+            console.log('silnik już był odpalony')
+        }
+    }
+
+    stopEngine() {
+        if (this.speed === 0) {
+            this.engine = false;
+            console.log('silnik zgaszony');
+        } else {
+            console.log('zatrzymaj się wpierw, zanim zgasisz silnik');
+        }
+    }
+
+    speedUp(amount) {
+        if (this.engine) {
+            this.speed = Math.min(this.speed + amount, this.maxSpeed);
+            console.log(`Twoja prędkość to: ${this.speed}`)
+        } else {
+            console.log('odpal silnik wpierw');
+        }
+    }
+
+    slowDown(amount) {
+        this.speed = Math.max(this.speed - amount, 0);
         console.log(`Twoja prędkość to: ${this.speed}`);
-    } else {
-        console.log('Odpal najpierw silnik');
     }
 }
 
-Auto.prototype.slowDown = function (amount) {
-    this.speed = Math.max(this.speed - amount, 0);
-    console.log(`Twoja prędkość to: ${this.speed}`)
+
+class Van extends Auto {
+    constructor(brand, maxSpeed, seats) {
+        super(brand, maxSpeed);
+        this.seats = seats;
+    }
+
+    speedUp(amount) {
+        super.speedUp(amount);
+        console.log('jedz bezpiecznie')
+    }
 }
+
+const bmw = new Van('bmw', 140, 7);
+const mercedes = new Auto('mercedes', 190);
 
 bmw.speedUp(200);
 bmw.startEngine();
 bmw.speedUp(40);
 bmw.speedUp(40);
 bmw.speedUp(240);
+bmw.speedUp(40);
+bmw.speedUp(40);
+bmw.speedUp(240);
 bmw.stopEngine();
 bmw.slowDown(50);
-bmw.slowDown(90);
+bmw.slowDown(250);
 bmw.stopEngine();
